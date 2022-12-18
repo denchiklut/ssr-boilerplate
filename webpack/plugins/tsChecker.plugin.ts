8c2db433ce6bbfb2d7d7146ba0c72c365e3f536a
daleksandrov <ollylut@gmail.com>
2022-12-18 14:16:42 +0100

    feat: comments - add comments

--- webpack/plugins/tsChecker.plugin.ts
+++ webpack/plugins/tsChecker.plugin.ts
@@ -1,6 +1,12 @@
 import { resolve } from 'path'
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 
+/**
+ * You can set `async` prop. If true, reports issues after webpack's compilation is done.
+ * Thanks to that it doesn't block the compilation. Used only in the watch mode.
+ * @default: compiler.options.mode === 'development'
+ * @see https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#options
+ */
 export const tsChecker = new ForkTsCheckerWebpackPlugin({
 	devServer: false,
 	typescript: {
