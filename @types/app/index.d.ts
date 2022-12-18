05d9a911491d0eb7dc2be7ef0e3b7ec48e94635e
daleksandrov <ollylut@gmail.com>
2022-12-18 14:07:42 +0100

    feat: comments - add comments

--- @types/app/index.d.ts
+++ @types/app/index.d.ts
@@ -1,3 +1,9 @@
+/**
+ *  If you have `imports/exports` statements
+ *  You put `Window` & `Express` declarations into `declare global {}`
+ *  @see https://www.typescriptlang.org/docs/handbook/declaration-merging.html
+ */
+
 interface Window {
 	IS_SERVER: boolean
 	IS_DEV: boolean
--- src/server/middleware/logger/index.ts
+++ src/server/middleware/logger/index.ts
@@ -1,5 +1,5 @@
 import morgan, { type StreamOptions } from 'morgan'
-import { logger as winstonLogger } from '../../utils'
+import { logger as winstonLogger } from 'server/utils'
 
 const stream: StreamOptions = {
 	write: message => winstonLogger.info(message)
