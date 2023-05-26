fefa1c5e87bbcbafe13720a43f08a13ae52c100e
denchiklut <ollylut@gmail.com>
2023-05-26 15:35:22 +0200

    fix: rate limit - adjust rate-limit

--- src/server/middleware/rate-limit/index.ts
+++ src/server/middleware/rate-limit/index.ts
@@ -2,6 +2,6 @@ import rateLimitMiddleware from 'express-rate-limit'
 
 export const rateLimit = rateLimitMiddleware({
 	skip: () => IS_DEV,
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 1 * 60 * 1000, // 1 minute
 	max: 100
 })
