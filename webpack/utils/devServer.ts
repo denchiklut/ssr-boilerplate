9ac8f920793f27d8322b582005ebfecf71f11f07
Karl Aleksandrov <ollylut@gmail.com>
2023-09-21 12:08:32 +0200

    fix: spa - add support for https

--- webpack/utils/devServer.ts
+++ webpack/utils/devServer.ts
@@ -1,10 +1,24 @@
 import { join } from 'path'
 import { ROOT_DIR } from './env'
+import { existsSync, readFileSync } from 'fs'
+
+const sslKeyPath = join(ROOT_DIR, 'certs/key.pem')
+const sslCertPath = join(ROOT_DIR, 'certs/cert.pem')
+const sslIsExist = existsSync(sslKeyPath) && existsSync(sslCertPath)
 
 /**
  * @see https://webpack.js.org/configuration/dev-server/
  */
 export const devServerConfig = {
+	server: {
+		type: sslIsExist ? 'https' : 'http',
+		options: {
+			...(sslIsExist && {
+				key: readFileSync(sslKeyPath),
+				cert: readFileSync(sslCertPath)
+			})
+		}
+	},
 	port: 3000,
 	host: 'localhost',
 	historyApiFallback: true,
