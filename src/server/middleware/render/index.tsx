228b94a6969c066b8dedbe3c78e68fd1ed80decd
Karl Aleksandrov <ollylut@gmail.com>
2024-01-22 18:18:20 +0100

    chore: format - run prettier

--- src/server/middleware/render/index.tsx
+++ src/server/middleware/render/index.tsx
@@ -1,9 +1,9 @@
-import { ChunkExtractor } from 'server/utils'
 import { renderToPipeableStream } from 'react-dom/server'
 import type { NextFunction, Request, Response } from 'express'
 import { StaticRouter } from 'react-router-dom/server'
-import { getApp, getStats } from './render.util'
 import { logger, setEnvVars } from 'src/common'
+import { ChunkExtractor } from 'server/utils'
+import { getApp, getStats } from './render.util'
 
 export const render = (req: Request, res: Response, next: NextFunction) => {
 	res.renderApp = () => {
