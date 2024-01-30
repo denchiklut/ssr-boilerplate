e7f0b5a651a2230dbd5512bdc7314a5668a3b316
Karl Aleksandrov <ollylut@gmail.com>
2024-01-30 12:34:27 +0100

    feat: pwa - update pwa setup to work with public_path external cdn

--- src/client/utils/pwa/pwa.util.ts
+++ src/client/utils/pwa/pwa.util.ts
@@ -1,12 +1,10 @@
 import { Workbox } from 'workbox-window'
-import { basePath } from 'src/common'
+import { basename, basePath } from 'src/common'
 
 export function registerSW(promptForUpdate: () => Promise<boolean>) {
 	if (!('serviceWorker' in navigator)) return
 
-	const wb = new Workbox(basePath('/service-worker.js'), {
-		scope: '/'
-	})
+	const wb = new Workbox(basePath('/service-worker.js'), { scope: basename })
 
 	wb.addEventListener('waiting', async () => {
 		wb.addEventListener('controlling', () => window.location.reload())
--- webpack/plugins/copy.plugin.ts
+++ webpack/plugins/copy.plugin.ts
@@ -3,10 +3,7 @@ import CopyPlugin from 'copy-webpack-plugin'
 import { IS_PROD, ROOT_DIR } from '../utils'
 
 const config = {
-	patterns: [
-		{ from: join(ROOT_DIR, 'assets/icons'), to: 'icons' },
-		{ from: join(ROOT_DIR, 'pwa/manifest.json'), to: 'pwa' }
-	]
+	patterns: [{ from: join(ROOT_DIR, 'assets/icons'), to: 'icons' }]
 }
 
 export const copyPlugin = IS_PROD && new CopyPlugin(config)
