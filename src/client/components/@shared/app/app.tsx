e76214599a8112d6b5122c41579f1cdf0f61372a
denchiklut <ollylut@gmail.com>
2024-09-28 12:12:19 -0700

    feat: env vars - protects secret from being exposed on client - only env vars starting from CLIET_ are exposed to the client

--- src/client/components/@shared/app/app.tsx
+++ src/client/components/@shared/app/app.tsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from 'react-helmet-async'
 
 export const App: FC<AppProps> = ({ children, nonce, helmetContext }) => {
 	__webpack_nonce__ = nonce
-	__webpack_public_path__ = getENV('PUBLIC_PATH')
+	__webpack_public_path__ = getENV('CLIENT_PUBLIC_PATH')
 
 	return (
 		<StrictMode>
--- src/common/env/env.util.ts
+++ src/common/env/env.util.ts
@@ -1,9 +1,12 @@
-import { decode } from '../decoder'
 import { string, mixed, object, type InferType } from 'yup'
+import { getOrDefault } from './get.util'
+import { createEnv } from './create-env'
+
+if (IS_SERVER) require('dotenv/config')
 
 const envSchema = object({
-	HOST: string().default('http://localhost:3000'),
-	PUBLIC_PATH: string().default('/'),
+	CLIENT_HOST: string().default('http://localhost:3000'),
+	CLIENT_PUBLIC_PATH: string().default('0.0.0'),
 	APP_VERSION: string().default('0.0.0'),
 	NODE_ENV: mixed<'production' | 'development' | 'test'>()
 		.oneOf(['production', 'development', 'test'])
@@ -12,15 +15,21 @@ const envSchema = object({
 
 export type Env = InferType<typeof envSchema>
 
-export const getENV = decode(envSchema, IS_SERVER || IS_SPA ? process.env : window.env_vars)
+export const getENV = getOrDefault(
+	createEnv({
+		clientPrefix,
+		schema: envSchema,
+		envs: IS_SERVER ? process.env : window.env_vars
+	})
+)
 
 export const setEnvVars = (nonce: string) => {
-	const { PUBLIC_PATH, HOST, APP_VERSION, NODE_ENV } = getENV()
+	const clientEnv = Object.entries(getENV())
+		.filter(([k]) => k.startsWith(clientPrefix))
+		.reduce<Collection<string, unknown>>((res, [k, v]) => {
+			res[k] = v
+			return res
+		}, {})
 
-	return `<script nonce='${nonce}'>window.env_vars = Object.freeze(${JSON.stringify({
-		HOST,
-		PUBLIC_PATH,
-		APP_VERSION,
-		NODE_ENV
-	})})</script>`
+	return `<script nonce='${nonce}'>window.env_vars = Object.freeze(${JSON.stringify(clientEnv)})</script>`
 }
