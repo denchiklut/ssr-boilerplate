ff5bee30dcbb5b8859234de2b28c03a9de193604
denchiklut <ollylut@gmail.com>
2024-10-13 13:24:05 -0700

    fix: serve public folder

--- src/server/router/static.ts
+++ src/server/router/static.ts
@@ -2,5 +2,5 @@ import { resolve } from 'path'
 import { Router, static as staticRoute } from 'express'
 
 export function staticRoutes(router: Router) {
-	router.use(staticRoute(IS_DEV ? 'assets' : resolve(__dirname, '../client')))
+	router.use(staticRoute(IS_DEV ? 'public' : resolve(__dirname, '../client')))
 }
--- webpack/plugins/html.plugin.ts
+++ webpack/plugins/html.plugin.ts
@@ -1,17 +1,19 @@
 import { join } from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import { ROOT_DIR, IS_PROD } from '../utils'
+import { ROOT_DIR, IS_PROD, PUBLIC_PATH } from '../utils'
 
 const offlineConfig = {
 	filename: 'pwa/offline.html',
 	inject: true,
-	template: join(ROOT_DIR, 'assets/offline/index.html')
+	template: join(ROOT_DIR, 'public/offline/index.html'),
+	publicPath: PUBLIC_PATH
 }
 
 const spaConfig = {
 	filename: 'index.html',
 	inject: true,
-	template: join(ROOT_DIR, 'assets/spa/index.html')
+	template: join(ROOT_DIR, 'public/spa/index.html'),
+	publicPath: PUBLIC_PATH
 }
 
 interface Props {
