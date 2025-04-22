baa3caa953801f928ad2b71d614fb981aa5e385c
denchiklut <ollylut@gmail.com>
2025-04-22 14:21:59 -0700

    update client logger in prod

--- src/common/logger/client/utils/print/index.ts
+++ src/common/logger/client/utils/print/index.ts
@@ -1,9 +1,11 @@
+import { getENV } from 'src/common/env'
 import type { Level } from '../../../types'
 import { css, colors } from './print.util'
 
 export function print() {
 	return function (_: object, propertyKey: string, descriptor: PropertyDescriptor) {
 		const originalMethod = descriptor.value
+		const isProd = getENV('NODE_ENV') === 'production'
 
 		descriptor.value = function (...args: unknown[]) {
 			const method = propertyKey as Level
@@ -12,12 +14,16 @@ export function print() {
 
 			if (typeof params[0] !== 'string') params.unshift('%o')
 
-			console[method](
-				`%c${ns}%c ${params[0]}`,
-				css(colors[method]).join(';'),
-				'color: inherit',
-				...params.slice(1)
-			)
+			if (isProd) {
+				console[method](...args)
+			} else {
+				console[method](
+					`%c${ns}%c ${params[0]}`,
+					css(colors[method]).join(';'),
+					'color: inherit',
+					...params.slice(1)
+				)
+			}
 
 			originalMethod.apply(this, args)
 		}
