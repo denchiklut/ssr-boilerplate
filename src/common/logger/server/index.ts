f0a0229a3f240b159e0117191e3208a7a2f79047
denchiklut <ollylut@gmail.com>
2026-07-09 20:20:12 -0700

    feat: logger src-attributes

--- src/common/logger/server/index.ts
+++ src/common/logger/server/index.ts
@@ -1,26 +1,31 @@
 import type { ILogger } from '../types'
+import { getSrc } from './utils'
 import { winstonLogger } from './winston'
 
 export class ServerLogger implements ILogger {
 	private logger = winstonLogger
 
 	log(...args: [string, unknown]) {
-		this.logger.log('lоg', ...args)
+		this.logger.notice(...args, this.src(this.log))
 	}
 
 	debug(...args: [string, unknown]) {
-		this.logger.debug(...args)
+		this.logger.debug(...args, this.src(this.debug))
 	}
 
 	info(...args: [string, unknown]) {
-		this.logger.info(...args)
+		this.logger.info(...args, this.src(this.info))
 	}
 
 	warn(...args: [string, unknown]) {
-		this.logger.warn(...args)
+		this.logger.warn(...args, this.src(this.warn))
 	}
 
 	error(...args: [string, unknown]) {
-		this.logger.error(...args)
+		this.logger.error(...args, this.src(this.error))
+	}
+
+	private src(caller: (...args: never[]) => unknown) {
+		if (IS_PROD) return getSrc(caller)
 	}
 }
